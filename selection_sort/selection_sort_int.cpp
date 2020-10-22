f4a721d57da4572270eeb6381731cb6787e32e19
Joseph Leakakos <jleakakos@gmail.com>
2020-10-21 19:56:29 -0600

    Added selection sort logic into c++ program

--- selection_sort/selection_sort_int.cpp
+++ selection_sort/selection_sort_int.cpp
@@ -4,7 +4,28 @@
 using namespace std;
 
 int main() {
-    vector<int> list {15,14,4,23,36,42,8,16,2,7,29,5};
+    vector<int> list {15,13,4,23,36,42,8,16,2,27,29,5};
+    cout << "Original List" << endl;
+    for (size_t num: list)
+        cout << num << endl;
+    
+    for (int i {0}; i < list.size() - 1; i++) {
+        size_t low = list.at(i);
+        size_t low_index = i;
+        
+        for (int j {i + 1}; j < list.size(); j++) {
+            if (list.at(j) < low) {
+                low = list.at(j);
+                low_index = j;
+            }
+        }
+        list.at(low_index) = list.at(i);
+        list.at(i) = low;
+    }
+
+    cout << endl << "Sorted List" << endl;
+    for (size_t num: list)
+        cout << num << endl;
 
 
 
